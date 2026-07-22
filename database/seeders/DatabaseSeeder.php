95466ee68ec395c115a2c1df6b05c756e4a2a73f
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2026-07-22 16:23:34 +0700

    fix: Update DatabaseSeeder to use updateOrCreate with correct parameters for admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::updateOrCreate([
-            'name' => 'admin',
-            'username' => 'admin',
-            'password' => bcrypt('admin12345'),
-            'role' => 'admin',
-        ]);
+        User::updateOrCreate(
+            ['username' => 'admin'],
+            [
+                'name' => 'admin',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+            ]
+        );
     }
 }
